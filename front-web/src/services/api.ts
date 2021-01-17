d33ef79626b5a04a050c333d7229c1cd79b45127
Gustavo <gustavonikov@gmail.com>
2021-01-17 06:33:08 -0300

    Updating server to heroku

--- front-web/src/services/api.ts
+++ front-web/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    // Change to localhost if you are in dev mode --> http://localhost:8080
+    baseURL: 'https://delivery-nikov.herokuapp.com',
 });
 
 export default api;
